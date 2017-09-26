39351a3a522eb9a5d437feb89d75ba55db450b98
wq601840968 <601840968@qq.com>
2017-09-26 16:52:21 +0800

    添加非模态对话框测试

---
+++ MFC_uart1/ClearDlg.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+// CClearDlg 对话框
+
+class CClearDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CClearDlg)
+
+public:
+	CClearDlg(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CClearDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_CLEAR };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
